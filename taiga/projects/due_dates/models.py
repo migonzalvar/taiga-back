d4c8df62b07e44c65ada2c482f1f7f238ad97c58
Miguel Gonzalez <migonzalvar@gmail.com>
2018-04-09 11:07:26 +0200

    wip: Add DueDateMixin to US, Task and Issue model

--- taiga/projects/due_dates/models.py
+++ taiga/projects/due_dates/models.py
@@ -20,6 +20,9 @@ class DueDateMixin(models.Model):
     due_date = models.DateField(
         blank=True, null=True, default=None, verbose_name=_('due date'),
     )
+    due_date_reason = models.TextField(
+        null=False, blank=True, default='', verbose_name=_('reason for the due date'),
+    )
 
     class Meta:
         abstract = True
