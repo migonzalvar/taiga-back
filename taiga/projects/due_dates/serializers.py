610c423ee1b268a96e9ee6ef4b53fef30175d18f
Miguel Gonzalez <migonzalvar@gmail.com>
2018-04-09 12:14:30 +0200

    wip: Add due_date_status method

--- taiga/projects/due_dates/serializers.py
+++ taiga/projects/due_dates/serializers.py
@@ -11,11 +11,29 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import datetime as dt
+
+from django.utils import timezone
 
 from taiga.base.api import serializers
-from taiga.base.fields import Field
+from taiga.base.fields import Field, MethodField
 
 
 class DueDateSerializerMixin(serializers.LightSerializer):
     due_date = Field()
     due_date_reason = Field()
+    due_date_status = MethodField()
+
+    THRESHOLD = 3
+
+    def get_due_date_status(self, obj):
+        if obj.due_date is None:
+            return 'not_set'
+        elif obj.is_closed:
+            return 'no_longer_applicable'
+        elif timezone.now().date() > obj.due_date:
+            return 'past_due'
+        elif (timezone.now().date() + dt.timedelta(days=self.THRESHOLD)) >= obj.due_date:
+            return 'due_soon'
+        else:
+            return 'set'
